e084d035588a947dbc27f660db48f1e37b3064a5
Ashith-VS <ashithvs855@gmail.com>
2024-09-10 10:32:53 +0530

    design completed

--- client/src/screens/Home/index.jsx
+++ client/src/screens/Home/index.jsx
@@ -26,7 +26,7 @@ const Home = () => {
 <Card/>
 <Card/>
 <Card/>
-    </Container>
+</Container>
   )
 }
 
--- client/src/screens/Video/index.jsx
+++ client/src/screens/Video/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import styled from 'styled-components';
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+import Comments from '../../components/Comments';
+import Card from '../../components/Card';
+
 const Container = styled.div`
   display: flex;
   gap: 24px;
@@ -102,9 +109,62 @@ const Video = () => {
     <Container>
       <Content>
         <VideoWrapper>
-            
+        <iframe
+            width="100%"
+            height="720"
+            src="https://www.youtube.com/embed/VUiKaAVZ2lc?si=ge7dPJuQWAgcxUhz"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
         </VideoWrapper>
+        <Title>Test Video</Title>
+        <Details>
+          <Info>7,948,154 views • Jun 22, 2022</Info>
+          <Buttons>
+            <Button>
+              <ThumbUpOutlinedIcon /> 123
+            </Button>
+            <Button>
+              <ThumbDownOffAltOutlinedIcon /> Dislike
+            </Button>
+            <Button>
+              <ReplyOutlinedIcon /> Share
+            </Button>
+            <Button>
+              <AddTaskOutlinedIcon /> Save
+            </Button>
+          </Buttons>
+        </Details>
+        <Hr />
+        <Channel>
+          <ChannelInfo>
+            <Image src="https://yt3.ggpht.com/yi/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffff-no-rj-mo" />
+            <ChannelDetail>
+              <ChannelName>njkhjh</ChannelName>
+              <ChannelCounter>200K subscribers</ChannelCounter>
+              <Description>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Doloribus laborum delectus unde quaerat dolore culpa sit aliquam
+                at. Vitae facere ipsum totam ratione exercitationem. Suscipit
+                animi accusantium dolores ipsam ut.
+              </Description>
+            </ChannelDetail>
+          </ChannelInfo>
+          <Subscribe>SUBSCRIBE</Subscribe>
+        </Channel>
+        <Hr />
+        <Comments/>
         </Content>
+        <Recommendation>
+          <Card type="sm" />
+          <Card type="sm" />
+          <Card type="sm" />
+          <Card type="sm" />
+          <Card type="sm" />
+          <Card type="sm" />
+        </Recommendation>
         </Container>
   )
 }
